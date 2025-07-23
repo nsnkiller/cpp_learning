e3719a5c58b0daadac11178da9fde6672ffebf36
nsnkiller <172902083@qq.com>
2025-07-23 15:42:13 +0800

    add 12.23 exercise

--- cpp_primer/mem_man.cpp
+++ cpp_primer/mem_man.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <vector>
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 
@@ -43,9 +44,38 @@ void f_2() {
     cout << "----------------------------------------" << endl << "Function f_2 left" << endl;
 }
 
+void f_12_23() {
+    // Example of a function that does nothing
+    cout << "----------------------------------------" << endl << "Function f_12_23 called" << endl;
+
+    const char *p = "Hello ";
+    const char *q = "World!";
+
+    char *r = new char[strlen(p) + strlen(q) + 1];
+    strcpy(r, p);
+    strcat(r, q);
+    cout << "Concatenated string: " << r << endl;
+
+    string s1 = "hello ";
+    string s2 = "world!";
+    string s3 = s1 + s2;
+
+    strcpy(r, s3.c_str()); // Copy the concatenated string into r
+    cout << "Concatenated string using std::string: " << r << endl;
+
+    delete[] r; // Free the allocated memory
+    cout << "Memory for concatenated string freed." << endl;
+
+
+    // This function is intentionally left empty
+    cout << "----------------------------------------" << endl << "Function f_12_23 left" << endl;
+}
+
+
 int main() {
     cout << "Memory Management Example" << endl;
     f_1(); // Call the function to demonstrate memory management
     f_2(); // Call the second function
+    f_12_23(); // Call the third function
     return 0;
 }
